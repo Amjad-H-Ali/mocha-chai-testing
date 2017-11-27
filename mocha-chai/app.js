e1d4aa72dcc072c5441e4a164e377969a7f03d54
Jim Haff <Jimmyhaff3@gmail.com>
2017-11-27 08:50:36 -0600

    required db

--- mocha-chai/app.js
+++ mocha-chai/app.js
@@ -9,7 +9,7 @@ var index = require('./routes/index');
 var books = require('./routes/book');
 
 var app = express();
-
+require('./db/db.js')
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
